d253b31ae40fcc577b07e0da028a8a1b86f7be90
Imran Iaskar <ilaskar@falabella.cl>
2020-07-26 18:51:19 +0530

    [Imran] add ipl data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    datasets: [],
+    iplSessions: []
+  },
+  mutations: {
+    setDatasets(state, datasets) {
+      state.datasets = datasets;
+    }
+  },
+  actions: {
+    fetchDatasets(store, url) {
+      Vue.http
+        .get(url)
+        .then((response) => {
+          store.commit('setDatasets', response.body);
+        })
+        .catch((error) => {
+          console.log(error.statusText);
+        });
+    }
+  },
   modules: {}
 });
