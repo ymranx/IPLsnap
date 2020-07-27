aa04992c306b8f6a102104fce6de0820c6d8a708
Imran Iaskar <ilaskar@falabella.cl>
2020-07-27 15:44:41 +0530

    [Imran] fix state issue

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,13 @@
-import { get } from 'lodash';
+// import { get } from 'lodash';
 export default {
   name: 'Dashboard',
   created() {
     //  console.log(this.$store.getters.getDataBySeason(2009));
   },
-  data() {
-    return {
-      seasons: get(this, ['$store', 'state', 'iplSeasons'], [])
-    };
+
+  computed: {
+    seasons() {
+      return this.$store.state.iplSeasons;
+    }
   }
 };
